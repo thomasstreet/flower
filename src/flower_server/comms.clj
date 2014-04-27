d741ad180147e8f370770f9dd5ff7390dd2965f8
David Graunke <graunked@gmail.com>
2014-04-27 15:15:27 -0700

    Add comms data

---
+++ src/flower_server/comms.clj
@@ -0,0 +1,15 @@
+(def boards {
+             :A [:flower :flower :flower :flower :flower :flower
+                 :vine :vine :vine :flower]
+             :B [:flower :flower :flower :flower :flower :flower
+                 :flower :flower :flower :flower]
+             :C [:flower :flower :flower :flower  :vine :flower ]
+             :D [:flower :flower :flower :flower :flower :flower
+                 :flower :flower]
+             :E [:flower :flower :vine :vine :vine :vine :vine]
+             :F [:flower :flower :flower :vine]
+             })
+
+(def writers {
+              :A (writer "/tmp/tty.usbmodem1411")
+              })
