d87890a53c7f06543d7bc7f090513caa15b7679d
David Graunke <graunked@gmail.com>
2014-06-15 15:32:08 -0700

    Add deps

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
+                 [org.rxtx/rxtx "2.1.7"]
+                 [incanter "1.5.5"]
                  [org.clojure/data.json "0.2.4"]]
   :plugins [[lein-ring "0.8.3"]]
   :ring {:handler flower-server.handler/app}
